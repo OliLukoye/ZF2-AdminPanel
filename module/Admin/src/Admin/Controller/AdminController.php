dc35f505104e791c438215347f561a064c36bf92
OliLukoye <vadwork@mail.ru>
2013-03-20 17:29:42 +0200

    Создание админки. Надо понять почему не запускается модуль 20.03.2013

--- module/Admin/src/Admin/Controller/AdminController.php
+++ module/Admin/src/Admin/Controller/AdminController.php
@@ -6,7 +6,21 @@ use Zend\View\Model\ViewModel;
 
 class AdminController extends AbstractActionController
 {
+    protected $albumTable;
+    
     public function indexAction()
     {
+        return new ViewModel(array(
+            'albums' => $this->getAlbumTable()->fetchAll(),
+        ));
+    }
+    
+    public function getAlbumTable()
+    {
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Admin\Model\AdminTable');
+        }
+        return $this->albumTable;
     }
 }
